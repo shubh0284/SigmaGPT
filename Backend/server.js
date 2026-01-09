51a50363e28c20fac4c01dfcbadacc687f174db5
shubhamshinare <shubhamshinare2004@gmail.com>
2026-01-09 18:43:59 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import OpenAI from 'openai';
+
+const client = new OpenAI({
+  apiKey: process.env['OPENAI_API_KEY'], // This is the default and can be omitted
+});
+
+const response = await client.responses.create({
+  model: 'gpt-4o',
+  instructions: 'You are a coding assistant that talks like a pirate',
+  input: 'Are semicolons optional in JavaScript?',
+});
+
+console.log(response.output_text);
