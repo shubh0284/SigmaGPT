d0e95b8541deb89f5b14aee10c777cf7f9d010cc
shubhamshinare <shubhamshinare2004@gmail.com>
2026-01-16 19:58:09 +0530

    Build a sidebar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
-import Sidebar from "./sidebar.jsx";
+import Sidebar from "./Sidebar.jsx";
 import ChatWindow from "./ChatWindow.jsx";
-import MyContext from "./MyContext.jsx";
+import { MyContext } from "./MyContext.jsx";
 
 function App() {
   const providerValues = {}; //value pass
   return (
     <div className="app">
-      <MyContext.Provider values={providerValues}>
+      <MyContext.Provider value={providerValues}>
         <Sidebar />
         <ChatWindow />
       </MyContext.Provider>
--- Frontend/src/Sidebar.jsx
+++ Frontend/src/Sidebar.jsx
@@ -1,7 +1,28 @@
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div>Sidebar</div>;
+  return (
+    <section>
+      {/* new chat button*/}
+      <button>
+        <img src="src/assets/blacklogo.png" alt="gpt logo"></img>
+        <i className="fa-solid fa-pen-to-square"></i>
+      </button>
+
+      {/*History*/}
+
+      <ul className="history">
+        <li>history 1</li>
+        <li>history 2</li>
+        <li>history 3</li>
+      </ul>
+
+      {/* sign */}
+      <div className="sign">
+        <p>By ShubhamShinare &hearts;</p>
+      </div>
+    </section>
+  );
 }
 
 export default Sidebar;
