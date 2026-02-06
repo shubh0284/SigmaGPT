d49a4906304880dd922dc81d7119d9dab37a060d
shubhamshinare <shubhamshinare2004@gmail.com>
2026-02-06 17:04:03 +0530

    Display the chats

--- Frontend/src/Chat.jsx
+++ Frontend/src/Chat.jsx
@@ -1,7 +1,29 @@
 import "./Chat.css";
+import { useContext } from "react";
+import { MyContext } from "./MyContext";
 
 function Chat() {
-  return <></>;
+  const { newChat, prevChats } = useContext(MyContext);
+
+  return (
+    <>
+      {newChat && <h1>Start a New Chat!</h1>}
+      <div className="chats">
+        {prevChats?.map((chat, idx) => (
+          <div
+            className={chat.role === "user" ? "userDiv" : "gptDiv"}
+            key={idx}
+          >
+            {chat.role === "user" ? (
+              <p className="userMessage">{chat.content}</p>
+            ) : (
+              <p className="gptMessage">{chat.content}</p>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Chat;
